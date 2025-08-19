bd9009bb1ac75ba04dfd25b4ca5d9fa160ecc875
isabellaaRamos <isabellaramos7@outlook.com>
2025-08-19 11:08:52 -0300

    commit

--- index.py
+++ index.py
@@ -2,14 +2,14 @@
 
 from scapy.all import *
 
-
+# Cria um pacote ICMP (ping) para o IP de teste
 pacote = IP(dst="8.8.8.8")/ICMP()
 
-
+# Envia e recebe respostas
 resposta = sr1(pacote, timeout=2)
 
 
-
+# Mostra a resposta
 if resposta:
     resposta.show()
 
@@ -25,4 +25,4 @@ else:
     resposta = requests.get(url)
 
     print("Status", resposta.status_code)
-    print
+    print("Comando", resposta.text[:200])  #Mostra só os 200 primeiros caracteres
